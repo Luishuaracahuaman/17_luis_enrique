ad4b75e9f2c580e4023a488446e0c3fdeab1ed0e
Luishuaracahuaman <luis.huaraca.h@vallegrande.edu.pe>
2026-03-19 10:04:57 -0500

    Primer guardado del proyecto

---
+++ src/test/java/ap1/luis/enrique/huraca/huaman/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ap1.luis.enrique.huraca.huaman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
